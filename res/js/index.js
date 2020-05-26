f3b9fe5b60860460030db77419b9478b40bc2588
twinspapa <twinspapa@monstercube.kr>
2020-05-26 15:13:36 +0900

    modify

--- res/js/index.js
+++ res/js/index.js
@@ -66,15 +66,25 @@ function cnt_load(obj_name, key_name) {
 
       $cnt_list.eq(1).attr('data-id',active_id).load("./cnt/"+ active_id + ".html");
       append_script(active_id);
+      append_css(active_id);
       $cnt_list.eq(0).attr('data-id',prev_id).load("./cnt/"+ prev_id + ".html");
       append_script(prev_id);
+      append_css(prev_id);
       $cnt_list.eq(2).attr('data-id',next_id).load("./cnt/"+ next_id + ".html");
       append_script(next_id);
+      append_css(next_id);
 }
+function append_css(id) {
+  var link_el = document.createElement('link');
+  link_el.rel="stylesheet";
+  link_el.href="./res/css/"+ id +".css";
 
+  document.head.appendChild(link_el);
+}
 function append_script(id) {
   var script_el = document.createElement('script');
 
   script_el.src="./res/js/"+ id +".js";
+  script_el.charset="utf-8"
   document.body.appendChild(script_el);
 }
